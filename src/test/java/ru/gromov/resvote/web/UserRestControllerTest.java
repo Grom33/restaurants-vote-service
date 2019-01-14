74f66e9b915f45a4b07723adff973a533c04da21
grom <mr.gromov.vitaly@gmail.com>
2019-01-14 20:10:20 +0300

    add userRestControllerTest

---
+++ src/test/java/ru/gromov/resvote/web/UserRestControllerTest.java
@@ -0,0 +1,43 @@
+package ru.gromov.resvote.web;
+
+import lombok.SneakyThrows;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/*
+ *   Created by Gromov Vitaly, 2019   e-mail: mr.gromov.vitaly@gmail.com
+ */
+public class UserRestControllerTest extends AbstractRestControllerTest{
+
+	private static final String LOGGED_USER_IVAN = "json/logged_user_ivan.json";
+	private static final String EDITED_USER= "json/edited_user.json";
+
+	@WithMockUser(value = "ivan@mail.ru")
+	@SneakyThrows
+	@Test
+	public void getLoggedUser() {
+		String json = util.getJsonString(util.getTestFile(LOGGED_USER_IVAN).toPath());
+		mockMvc.perform(get(REST_URL + "users"))
+				.andExpect(status().isOk())
+				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+				.andExpect(content().json(json));
+	}
+
+	@WithMockUser(value = "ivan@mail.ru")
+	@SneakyThrows
+	@Test
+	public void updateLoggedUser() {
+		String json = util.getJsonString(util.getTestFile(EDITED_USER).toPath());
+		mockMvc.perform(put(REST_URL + "users")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json))
+				.andExpect(status().isOk());
+	}
+}
